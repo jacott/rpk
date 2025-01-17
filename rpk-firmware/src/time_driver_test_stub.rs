3c5438d60456da0dfe4b4a7476a307419be91a0f
Geoff Jacobsen <geoffjacobsen@gmail.com>
2025-01-17 19:58:19 +1300

    deps: update embassy

--- rpk-firmware/src/time_driver_test_stub.rs
+++ rpk-firmware/src/time_driver_test_stub.rs
@@ -1,10 +1,10 @@
 extern crate std;
+
 use core::{cell::RefCell, task::Waker};
-use embassy_time_driver::{AlarmHandle, Driver};
-use embassy_time_queue_driver::TimerQueue;
+use embassy_time_driver::Driver;
 use std::time::SystemTime;
 
-pub(crate) struct TestTimeDriver;
+struct TestTimeDriver;
 
 impl Driver for TestTimeDriver {
     fn now(&self) -> u64 {
@@ -19,21 +19,15 @@ impl Driver for TestTimeDriver {
             }
         })
     }
-    unsafe fn allocate_alarm(&self) -> Option<AlarmHandle> {
-        std::unimplemented!()
-    }
-    fn set_alarm_callback(&self, alarm: AlarmHandle, callback: fn(*mut ()), ctx: *mut ()) {
-        let _ = (alarm, callback, ctx);
-        std::unimplemented!()
-    }
-    fn set_alarm(&self, alarm: AlarmHandle, timestamp: u64) -> bool {
-        let _ = (alarm, timestamp);
-        std::unimplemented!()
+
+    fn schedule_wake(&self, _at: u64, waker: &Waker) {
+        let waker = waker.clone();
+        waker.wake();
     }
 }
 
 impl TestTimeDriver {
-    pub(crate) fn set_time(&self, t: u64) {
+    pub fn set_time(&self, t: u64) {
         NOW.with_borrow_mut(|now| *now = t);
     }
 }
@@ -48,14 +42,3 @@ embassy_time_driver::time_driver_impl!(static TIME_DRIVER: TestTimeDriver = Test
 pub fn set_time(t: u64) {
     TIME_DRIVER.set_time(t);
 }
-
-struct MyTimerQueue; // not public!
-
-impl TimerQueue for MyTimerQueue {
-    fn schedule_wake(&'static self, _at: u64, waker: &Waker) {
-        let waker = waker.clone();
-        waker.wake();
-    }
-}
-
-embassy_time_queue_driver::timer_queue_impl!(static QUEUE: MyTimerQueue = MyTimerQueue);
