92245b2e2634fa985b9cef0916df98265adcb453
Geoff Jacobsen <geoffjacobsen@gmail.com>
2025-11-11 10:39:04 +1300

    feat: allow setting lag on timer wait

--- rpk-firmware/src/time_driver_test_stub.rs
+++ rpk-firmware/src/time_driver_test_stub.rs
@@ -9,38 +9,39 @@ struct TestTimeDriver;
 impl Driver for TestTimeDriver {
     fn now(&self) -> u64 {
         NOW.with_borrow(|now| {
-            if *now == 0 {
+            if now.0 == 0 {
                 SystemTime::now()
                     .duration_since(SystemTime::UNIX_EPOCH)
                     .unwrap()
                     .as_micros() as u64
             } else {
-                *now
+                now.0
             }
         })
     }
 
     fn schedule_wake(&self, at: u64, waker: &Waker) {
-        set_time(at);
+        NOW.with_borrow_mut(|now| {
+            if now.0 != 0 && at > now.0 {
+                now.0 = at + now.1;
+            }
+        });
 
-        let waker = waker.clone();
-        waker.wake();
-    }
-}
-
-impl TestTimeDriver {
-    pub fn set_time(&self, t: u64) {
-        NOW.with_borrow_mut(|now| *now = t);
+        waker.wake_by_ref();
     }
 }
 
 std::thread_local! {
-    static NOW: RefCell<u64> = const {RefCell::new(0)};
+    static NOW: RefCell<(u64,u64)> = const {RefCell::new((0,0))};
 }
 
 // TODO this needs to be thread local
 embassy_time_driver::time_driver_impl!(static TIME_DRIVER: TestTimeDriver = TestTimeDriver);
 
 pub fn set_time(t: u64) {
-    TIME_DRIVER.set_time(t);
+    NOW.with_borrow_mut(|now| now.0 = t);
+}
+
+pub fn set_wait_lag(t: u64) {
+    NOW.with_borrow_mut(|now| now.1 = t);
 }
